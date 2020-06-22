6f60b040166171a5613f023db8b1e45db50de10f
ettienekorayyi <corralstephenmelben@gmail.com>
2020-06-22 23:07:28 +1000

    Seeded the Values database.

--- API/Program.cs
+++ API/Program.cs
@@ -6,9 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API
 {
@@ -16,19 +13,7 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using(var scope = host.Services.CreateScope()) {
-                var services = scope.ServiceProvider;
-                try {
-                    var context = services.GetRequiredService<DataContext>();
-                    context.Database.Migrate();
-                } catch(Exception ex) { 
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger .LogError(ex, "An error occured during migration.");
-                }
-            }
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
