9bcd90d2aed8da0116c660746cc6aa940944b0fd
ettienekorayyi <corralstephenmelben@gmail.com>
2020-07-26 20:30:11 +1000

    Added selection feature

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { Grid, List } from "semantic-ui-react";
 import { IActivity } from "../../../models/activity";
+import { ActivityList } from "./ActivityList";
+import { ActivityDetails } from "../details/ActivityDetails";
+import { ActivityForm } from "../../activities/form/ActivityForm";
 
 interface IProps {
-    activities: IActivity[];
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
 }
 
-export const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
+export const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
+        <ActivityList selectActivity={selectActivity} activities={activities} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        { selectedActivity && <ActivityDetails activity={selectedActivity} />}
+        <ActivityForm />
       </Grid.Column>
     </Grid>
   );
