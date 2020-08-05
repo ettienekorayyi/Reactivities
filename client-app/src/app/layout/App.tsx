cf1e1d0f9da82fa94ac8d4c927292eee060576ab
ettienekorayyi <corralstephenmelben@gmail.com>
2020-08-05 19:14:51 +1000

    DEV-081-Setting-Up-MobX-Store

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from "react";
+import React, {
+  useState,
+  useEffect,
+  Fragment,
+  SyntheticEvent,
+  useContext,
+} from "react";
 import axios from "axios";
 import { Container } from "semantic-ui-react";
 
@@ -6,10 +12,13 @@ import "./styles.css";
 import NavBar from "../../features/nav/NavBar";
 import { IActivity } from "../../models/activity";
 import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
-import agent from "./api/agent";
+import agent from "../api/agent";
 import { LoadingComponent } from "./LoadingComponent";
+import ActivityStore from "../stores/activityStore";
+import { observer } from "mobx-react-lite";
 
 const App = () => {
+  const activityStore = useContext(ActivityStore);
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [submitting, setSubmitting] = useState<boolean>(false);
@@ -17,7 +26,7 @@ const App = () => {
     null
   );
   const [editMode, setEditMode] = useState(false);
-  const [target, setTarget] = useState('');
+  const [target, setTarget] = useState("");
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter((a) => a.id === id)[0]);
@@ -31,54 +40,55 @@ const App = () => {
 
   const handleCreateActivity = (activity: IActivity) => {
     setSubmitting(true);
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setSubmitting(false));
+    agent.Activities.create(activity)
+      .then(() => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
   };
 
   const handleEditActivity = (activity: IActivity) => {
     setSubmitting(true);
-    agent.Activities.update(activity).then(() => {
-      setActivities([
-        ...activities.filter((a) => a.id !== activity.id),
-        activity,
-      ]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setSubmitting(false));
+    agent.Activities.update(activity)
+      .then(() => {
+        setActivities([
+          ...activities.filter((a) => a.id !== activity.id),
+          activity,
+        ]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
   };
 
-  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
+  const handleDeleteActivity = (
+    event: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) => {
     setSubmitting(true);
     setTarget(event.currentTarget.name);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter((a) => a.id !== id)]);
-    }).then(() => setSubmitting(false));
-    
+    agent.Activities.delete(id)
+      .then(() => {
+        setActivities([...activities.filter((a) => a.id !== id)]);
+      })
+      .then(() => setSubmitting(false));
   };
 
   useEffect(() => {
-    agent.Activities.list().then((response) => {
-      let activities: IActivity[] = [];
-      response.forEach((activity) => {
-        activity.date = activity.date.split(".")[0];
-        activities.push(activity);
-      });
-      setActivities(activities);
-    }).then(() => setLoading(false));
-  }, []);
+    activityStore.loadActivities();
+  }, [activityStore]);
 
-  if(loading) return <LoadingComponent content='Loading Activities...' />
+  if (activityStore.loadingInitial) return <LoadingComponent content="Loading Activities..." />;
 
   return (
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
       <Container style={{ marginTop: "7em" }}>
         <ActivityDashboard
+          activities={activityStore.activities}
           selectActivity={handleSelectActivity}
-          activities={activities}
           selectedActivity={selectedActivity}
           editMode={editMode}
           setEditMode={setEditMode}
@@ -94,4 +104,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default observer(App);
