0ec9e4127e1ef7a7f56b596345b2a725a5cf663f
ettienekorayyi <corralstephenmelben@gmail.com>
2020-08-12 14:00:43 +1000

    Added Links to Activity Details

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -16,6 +16,7 @@ import { observer } from "mobx-react-lite";
 import { HomePage } from "../../features/home/HomePage";
 import { ActivityForm } from "../../features/activities/form/ActivityForm";
 import { Route } from "react-router-dom";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 const App = () => {
   const activityStore = useContext(ActivityStore);
@@ -32,6 +33,7 @@ const App = () => {
       <Container style={{ marginTop: "7em" }}>
         <Route exact path='/' component={HomePage} />
         <Route path='/activities' component={ActivityDashboard} />
+        <Route path='/activities/:id' component={ActivityDetails} />
         <Route path='/createActivity' component={ActivityForm} />
       </Container>
     </Fragment>
