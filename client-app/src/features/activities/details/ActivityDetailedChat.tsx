b5868665711887dfcaa9c4bd0400f4bb595da07c
ettienekorayyi <corralstephenmelben@gmail.com>
2020-08-15 19:12:31 +1000

    Styled the Activity Detailed Page Header

---
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ActivityDetailedChat = () => {
+    return (
+        <div>
+            <h1>Chat</h1>
+        </div>
+    )
+}
+export default ActivityDetailedChat;
