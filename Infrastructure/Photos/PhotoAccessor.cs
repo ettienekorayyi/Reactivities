67fb698e57ad14a30507a6e477740334bb2010c1
ettienekorayyi <corralstephenmelben@gmail.com>
2020-10-20 16:31:06 +1100

    Added PhotoAccessor class

--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -1,7 +1,19 @@
+using Application.Interface;
+using Application.Photos;
+using Microsoft.AspNetCore.Http;
+
 namespace Infrastructure.Photos
 {
-    public class PhotoAccessor
+    public class PhotoAccessor : IPhotoAccessor
     {
-        
+        public PhotoUploadResult AddPhoto(IFormFile file)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string DeletePhoto(string publicId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
