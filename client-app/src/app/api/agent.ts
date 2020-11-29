698f147ba680489f2249f204b165f405688a4d4e
ettienekorayyi <corralstephenmelben@gmail.com>
2020-11-29 19:37:56 +1100

    refactored the activityStore, agent.ts and activity.ts for paging

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,6 @@
 import { IUserFormValues } from './../../models/user';
 import { history } from '../..';
-import { IActivity } from '../../models/activity';
+import { IActivitiesEnvelope, IActivity } from '../../models/activity';
 import axios, { AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { IUser } from '../../models/user';
@@ -10,25 +10,25 @@ axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.request.use((config) => {
     const token = window.localStorage.getItem('jwt');
-    if(token) config.headers.Authorization = `Bearer ${token}`;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 }, error => {
     return Promise.reject(error);
 });
 
 axios.interceptors.response.use(undefined, error => {
-    if(error.message === 'Network Error' && !error.response) {
+    if (error.message === 'Network Error' && !error.response) {
         toast.error('Network error - make sure API is running.');
     }
     const { status, data, config } = error.response;
-    
-    if(status === 404) {
+
+    if (status === 404) {
         history.push('/notfound');
     }
-    if(status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
+    if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
         history.push('/notfound');
     }
-    if(status === 500) {
+    if (status === 500) {
         toast.error('Server Error - check the terminal for more info!');
     }
     throw error.response;
@@ -37,14 +37,14 @@ axios.interceptors.response.use(undefined, error => {
 const responseBody = (response: AxiosResponse) => response.data;
 
 // currying
-const sleep = (ms: number) => (response: AxiosResponse) => 
+const sleep = (ms: number) => (response: AxiosResponse) =>
     new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms))
 
 const requests = {
     get: (url: string) =>
-     axios
-        .get(url)
-        .then(responseBody),
+        axios
+            .get(url)
+            .then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
     del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
@@ -52,16 +52,17 @@ const requests = {
         let formData = new FormData();
         formData.append('File', file);
         return axios.post(url, formData, {
-            headers: {'Content-type': 'multipart/form-data'}
+            headers: { 'Content-type': 'multipart/form-data' }
         }).then(responseBody);
     }
 }
 
 const Activities = {
-    list: (): Promise<IActivity[]> => requests.get('/activities'),
+    list: (limit?: number, page?: number): Promise<IActivitiesEnvelope> => 
+        requests.get(`/activities?limit=${limit}&offset=${page ? page * limit! : 0}`),
     details: (id: string) => requests.get(`/activities/${id}`),
     create: (activity: IActivity) => requests.post('/activities', activity),
-    update: (activity: IActivity) => requests.put(`/activities/${ activity.id}`, activity),
+    update: (activity: IActivity) => requests.put(`/activities/${activity.id}`, activity),
     delete: (id: string) => requests.del(`/activities/${id}`),
     attend: (id: string) => requests.post(`/activities/${id}/attend`, {}),
     unattend: (id: string) => requests.del(`/activities/${id}/attend`)
@@ -70,13 +71,13 @@ const Activities = {
 // Encapsulates the get and post request in a User object for current user, login and register.
 const User = {
     current: (): Promise<IUser> => requests.get('/user'),
-    login: (user : IUserFormValues): Promise<IUser> => requests.post('/user/login', user),
-    register: (user : IUserFormValues): Promise<IUser> => requests.post('/user/register', user),
+    login: (user: IUserFormValues): Promise<IUser> => requests.post('/user/login', user),
+    register: (user: IUserFormValues): Promise<IUser> => requests.post('/user/register', user),
 }
 
 const Profiles = {
     get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`),
-    update: (displayName: string ,bio: string ) => requests.put(`/profiles/`, { displayName, bio}),
+    update: (displayName: string, bio: string) => requests.put(`/profiles/`, { displayName, bio }),
     uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
     deletePhoto: (id: string) => requests.del(`/photos/${id}`),
